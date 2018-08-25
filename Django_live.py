143b33c927b4bd9b7b56b85ad555eb5864a657c8
Sibiminifly <anna.sibila@gmail.com>
2018-08-25 16:52:07 +0200

    My Django Girls app, f-st commit

---
+++ Django_live.py
@@ -0,0 +1,27 @@
+for i in range(1,6,2):
+    print(i)
+
+
+
+#
+# dziewczyny = ['Rachel', 'Monica', 'Phoebe', 'Ola', 'Ty']
+#
+#
+# for imie in dziewczyny:
+#     imie = input('Podaj imie\n')
+#
+#     def hi(imie):
+#         if imie == 'Ola':
+#             print('Hey Ola')
+#         elif imie == 'Aga':
+#             print('Strzalka Aga')
+#         else:
+#             print('Hej ' + imie + '!')
+#
+#     hi(imie)
+#     print('Kolejna dziewczyna')
+# print(dziewczyny)
+#
+# def hi(imie):
+#     print('Hej ' + imie + '!')
+# hi("Rachel")
